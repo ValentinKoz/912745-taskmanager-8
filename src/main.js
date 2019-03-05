9be60df414f7bd1872ccfaf6b35ca35722590c53
Valentin Kozhin <kozhin_val@mail.ru>
2019-03-06 02:58:54 +0300

    Исправлены ошибки в названии

--- src/main.js
+++ src/main.js
@@ -1,25 +1,25 @@
 import makeFilter from './make-filter.js';
 import getCard from './make-task.js';
 import makeTask from './get-task.js';
-import info from './info.js';
+import getTask from './info.js';
 
 const mainFilter = document.querySelector(`.main__filter`);
 const boardTasks = document.querySelector(`.board__tasks`);
 let conteinerFilters = [];
 
-for (const Filter of info.Filters) {
-  conteinerFilters.push(makeFilter(Filter, info.rand()));
+for (const Filter of getTask.Filters) {
+  conteinerFilters.push(makeFilter(Filter, getTask.rand()));
 }
 conteinerFilters = conteinerFilters.join(` `);
 mainFilter.insertAdjacentHTML(`beforeend`, conteinerFilters);
 
 let conteinerCards = [];
-info.addingCards(info, getCard, makeTask, conteinerCards, boardTasks);
+getTask.addingCards(getTask, getCard, makeTask, conteinerCards, boardTasks);
 
 mainFilter.addEventListener(`change`, function (evt) {
   if (evt.target.tagName === `INPUT`) {
     boardTasks.innerText = ``;
-    const cardCount = info.rand();
+    const cardCount = getTask.rand();
     for (let i = 0; i < cardCount; i++) {
       boardTasks.insertAdjacentHTML(`beforeend`, getCard(makeTask()));
     }
